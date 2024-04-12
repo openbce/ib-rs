c7edfd3d585d9ee9bdbc8b65f2cb0d98a2c1d83b
Klaus Ma <klausm@nvidia.com>
2024-04-12 07:11:07 +0000

    fmt output.
    Signed-off-by: Klaus Ma <klausm@nvidia.com>

--- ufmcli/src/info.rs
+++ ufmcli/src/info.rs
@@ -4,13 +4,13 @@ pub async fn run(conf: UFMConfig) -> Result<(), UFMError> {
     let ufm = libufm::connect(conf)?;
     let config = ufm.get_configuration().await?;
 
-    println!("subnet prefix: {}", config.subnet_prefix);
-    println!("m_key: {}", config.m_key);
-    println!("m_key_per_port: {}", config.m_key_per_port);
-    println!("sm_key: {}", config.sm_key);
-    println!("sa_key: {}", config.sa_key);
-    println!("log_file: {}", config.log_file);
-    println!("qos: {}", config.qos);
+    println!("subnet prefix  : {}", config.subnet_prefix);
+    println!("m_key          : {}", config.m_key);
+    println!("m_key_per_port : {}", config.m_key_per_port);
+    println!("sm_key         : {}", config.sm_key);
+    println!("sa_key         : {}", config.sa_key);
+    println!("qos            : {}", config.qos);
+    println!("log_file       : {}", config.log_file);
 
     Ok(())
 }
