1f50cee4660453f9a6cd48ea5dcd265b8803f1ce
Igor Ivanov <igori@nvidia.com>
2024-11-15 11:44:57 +0200

    feat: update create command to use set_partition()
    Signed-off-by: Igor Ivanov <igori@nvidia.com>

--- ufmctl/src/create.rs
+++ ufmctl/src/create.rs
@@ -1,4 +1,4 @@
-use libufm::{Partition, PartitionKey, PortConfig, PortMembership, UFMConfig, UFMError};
+use libufm::{Partition, PartitionKey, PartitionQoS, PortConfig, PortMembership, UFMConfig, UFMError};
 
 pub struct CreateOptions {
     pub pkey: String,
@@ -6,6 +6,9 @@ pub struct CreateOptions {
     pub index0: bool,
     pub membership: String,
     pub guids: Vec<String>,
+    pub mtu: u16,
+    pub service_level: u8,
+    pub rate_limit: f64,
 }
 
 pub async fn run(conf: UFMConfig, opt: &CreateOptions) -> Result<(), UFMError> {
@@ -24,10 +27,14 @@ pub async fn run(conf: UFMConfig, opt: &CreateOptions) -> Result<(), UFMError> {
         name: "".to_string(),
         pkey: PartitionKey::try_from(opt.pkey.clone())?,
         ipoib: opt.ipoib,
-        qos: Default::default(),
+        qos: PartitionQoS {
+            mtu_limit: opt.mtu,
+            service_level: opt.service_level,
+            rate_limit: opt.rate_limit,
+        },
     };
 
-    ufm.bind_ports(p, pbs).await?;
+    ufm.set_partition(p, pbs).await?;
 
     Ok(())
 }
